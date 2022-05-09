753342924cc63fa0008e0e315e338af5bebf5fdb
Dhruv Mathur <dhruv.mathur03@gmail.com>
2022-05-09 18:02:14 -0400

    wrote function to find team page

--- scrape.py
+++ scrape.py
@@ -1,20 +1,31 @@
+from random import betavariate
 from bs4 import BeautifulSoup
 import requests
 
 url = 'https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures'
-data = requests.get(url)
 
-req_data = []
-my_lst = []
-raw = BeautifulSoup(data.text, 'html.parser')
-body = raw.find(lambda tag: tag.name == 'tbody')
+req_data = {
+            "Total Goals For" : 0,
+            "Total Goals Against" : 0,
+            "Goals Scored Against Opponent" : 0,
+            "Goals Conceded Against Opponent" : 0
+           }
 
-rows = body.find_all('tr')
-for i in rows:
-    col = i.find(lambda tag: tag.name == "td" and tag.has_attr('data-stat') and tag['data-stat'] == 'squad_a')
-    my_lst.append(col)
+def find_team_page(team_name):
+    data = requests.get(url)
+    raw = BeautifulSoup(data.text, 'html.parser')
+    body = raw.find(lambda tag: tag.name == 'tbody')
+    rows = body.find_all('tr')
+    my_lst = []
 
-for i in my_lst:
-    a = i.find(lambda tag: tag.name == "a" and "Brentford" in tag["href"])
-    print(a)
-    break
+    for i in rows:
+        col = i.find(lambda tag: tag.name == "td" and tag.has_attr('data-stat') and tag['data-stat'] == 'squad_a')
+        my_lst.append(col)
+
+    for i in my_lst:
+        a = i.find(lambda tag: tag.name == "a" and team_name in tag["href"])
+        if a:
+            new_url = a["href"]
+            break
+    
+    return new_url
